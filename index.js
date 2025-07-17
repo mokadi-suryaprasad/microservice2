b9e4b4c20d248370619e758b5068547db46bcb62
Mokadi Surya Prasad <90204775+mokadi-suryaprasad@users.noreply.github.com>
2025-07-17 12:04:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 80;
 
 app.get('/', (req, res) => {
     console.log('Request URL:', req.url);
-    res.send('<h1>microservice2!</h1>');
+    res.send('<h1>microservice22!</h1>');
 });
 
 app.listen(port, () => {
